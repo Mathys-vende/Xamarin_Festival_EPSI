fabb55b24bf1f21ef30e88d88227449b81ec0aa6
Mathys VENDE <mathys.vende@armor-group.com>
2021-12-06 11:14:52 +0100

    Login complétement fonctionnel

---
+++ plz/plz/Services/LoginService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace plz.Services
+{
+    public class LoginService
+    {
+        private const string LoginWebServiceUrl = "http://localhost:7016/api/Admin/";
+
+        public async Task<bool> checkLogin(string email, string password)
+        {
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(LoginWebServiceUrl + email + "/" + password);
+
+            if(response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        public async Task<bool> CheckLoginIfExists(string email, string password)
+        {
+            var check = await checkLogin(email, password);
+
+            return check;
+        }
+    }
+}
